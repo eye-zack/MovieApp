b14ed04ebbfb09062dc09644bf4f0fe11fd488a8
eye-zack <97070050+eye-zack@users.noreply.github.com>
2023-05-30 22:54:47 -0500

    Added pipes and search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
+import { SearchComponent } from './search/search.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SearchComponent, PipesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
